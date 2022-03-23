3366f4ffa054f44e16885c62fcfd65786116b829
Dorian C <thenewdorian21@gmail.com>
2022-03-23 00:58:52 -0600

    made exception

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
 ﻿
+
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Welcome. please enter your number");
+        ReadInt(Console.ReadLine());
+
+    }
+
+    public int ReadInt(string input)
+    {
+       try
+       {
+           return int.Parse(input);
+       }
+       catch (Exception e)
+       {
+           System.Console.WriteLine(e.Message);
+       }
+        
+    }
+}
