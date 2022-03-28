b15fd27045f842d98a4a9cdbcf44b0cf7dcccf46
Dorian C <thenewdorian21@gmail.com>
2022-03-28 07:53:58 -0600

    fdsf

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSyncfusionBlazor();
+// builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
