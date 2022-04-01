23c40f9e53add51bc1a91e93ceb357f8b87a2c84
Dorian C <thenewdorian21@gmail.com>
2022-04-01 07:53:52 -0600

    started with file reading

--- ClassLibrary/Course.cs
+++ ClassLibrary/Course.cs
@@ -2,8 +2,10 @@ namespace FinalProject;
 
 public class Course
 {
-    public Course(string courseName, weekday courseDays, DateTime timeStart, DateTime timeEnd, Professor professor)
+    List<Course> AllCourses = new List<Course>();
+    public Course(string crn, string courseName, weekday courseDays, DateTime timeStart, DateTime timeEnd, Professor professor)
     {
+        string CRN = crn; //CRN = Course Regisration Number
         string CourseName = courseName; //I want this to be changeable but not by the UI
         weekday CourseDays = courseDays;
         DateTime TimeStart = timeStart;
@@ -11,9 +13,17 @@ public class Course
         Professor Instructor = professor;
     } //Use method overflow or whatever to make it so that courses can have up to 5 possible weekdays
 
-    public Course(string courseName)
+
+
+    public Course(string courseName) //this is mostly for testing purposes.
     {
         string CourseName = courseName;
     }
+
+    // List<Course> MasterCourseListReader()
+    // {
+    //     //filestream etc etc
+    //     //return list of literally every single course
+    // }
 
 }
