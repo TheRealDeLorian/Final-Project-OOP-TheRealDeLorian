cbeb4a479002ebfbdff13ce6af1bc1d5f74df129
Dorian C <thenewdorian21@gmail.com>
2022-04-02 13:56:07 -0600

    is anything working

---
+++ ConsoleUI/FinalProject.cs
@@ -0,0 +1,20 @@
+﻿namespace FinalProject;
+
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Welcome. Are you ready to make a class schedule? Please enter your Student ID");
+        string id = Console.ReadLine();
+        
+        
+
+    }
+
+    
+
+
+    
+}
+
