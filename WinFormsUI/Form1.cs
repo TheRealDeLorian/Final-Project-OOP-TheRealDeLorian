c3afc73d98c779c9ffbad13cb9863cabba9cde6a
Dorian C <thenewdorian21@gmail.com>
2023-03-24 12:18:26 -0600

    added some winforms stuff

--- WinFormsUI/Form1.cs
+++ WinFormsUI/Form1.cs
@@ -1,10 +1,32 @@
+using FinalProject;
+
 namespace WinFormsUI
 {
     public partial class Form1 : Form
     {
+        internal static string ID;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IDInputBoxNumbers_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubmitIDbtn_Click(object sender, EventArgs e)
+        {
+            ID = txtBoxID.Text;
+            MessageBox.Show(ID);
+        }
+
+        
     }
 }
--- WinFormsUI/Program.cs
+++ WinFormsUI/Program.cs
@@ -1,17 +1,26 @@
+using FinalProject;
+using Microsoft.VisualBasic;
+
 namespace WinFormsUI
 {
-    internal static class Program
+    public static class Program
     {
+        static Student newStudent;
+        static int ID;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            Form1 f1 = new Form1();
+            f1.Show();
+
+            
+
+            
         }
     }
 }
