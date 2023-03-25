0e507da27b13bb7823c4182910ce7bfcdcc90d9a
Dorian C <thenewdorian21@gmail.com>
2023-03-25 12:07:00 -0600

    prints schedule to box

--- WinFormsUI/ScheduleMenu.cs
+++ WinFormsUI/ScheduleMenu.cs
@@ -20,11 +20,32 @@ namespace WinFormsUI
             InitializeComponent();
             ID = Student.studentID;
 
-            txtbxSchedule.Text = DataMan.courseList.ToString();
+            txtbxSchedule.Text = PrintSchedule();
 
-            
+
         }
 
+        public string PrintSchedule()
+        {
+            string courseInfo = "";
+            foreach (Course course in DataMan.courseList)
+            {
+                string days = "";
+                foreach (string day in course.Days)
+                {
+                    days = days + $"{day}, ";
+                }
+                courseInfo = courseInfo +
+
+                    $"Course Name: {course.CourseName}\r\n" +
+                    $"CRN: {course.CRN}\r\n" +
+                    $"Days offered: {days}\r\n" +
+                    $"from {course.TimeStart} to {course.TimeEnd}\r\n" +
+                    $"Description: {course.Description}\r\n" +
+                    "\r\n"; //to seperate the courses
+            }
+            return courseInfo;
+        }
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -44,7 +65,7 @@ namespace WinFormsUI
 
         private void button1_Click_1(object sender, EventArgs e) //erases everything 
         {
-            
+
             File.WriteAllText(Path.Combine("Courses", $"{ID}.txt"), string.Empty);
             File.Delete(Path.Combine("Courses", $"{ID}.txt"));
             DataMan.courseDict.Clear();
