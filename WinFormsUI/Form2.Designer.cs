a5ea3f50d75b1b11d06342b77ac7a8e58f1a7216
Dorian C <thenewdorian21@gmail.com>
2023-03-25 10:00:59 -0600

    made some changes

--- WinFormsUI/Form2.Designer.cs
+++ WinFormsUI/Form2.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddCourse = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(184, 62);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(398, 304);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(412, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = $"Welcome, student {ID} Here\'s what your schedule looks like for this semester:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnAddCourse
+            // 
+            this.btnAddCourse.Location = new System.Drawing.Point(184, 389);
+            this.btnAddCourse.Name = "btnAddCourse";
+            this.btnAddCourse.Size = new System.Drawing.Size(96, 23);
+            this.btnAddCourse.TabIndex = 2;
+            this.btnAddCourse.Text = "Add course";
+            this.btnAddCourse.UseVisualStyleBackColor = true;
+            this.btnAddCourse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(536, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Erase schedule and start over";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(483, 415);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "(This will remove all courses!)";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAddCourse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private Button btnAddCourse;
+        private Button button1;
+        private Label label2;
     }
 }
